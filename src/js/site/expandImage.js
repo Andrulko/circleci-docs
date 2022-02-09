05b78e08e5b22ccad34511f1417c7c5c206b6949
chantelle g <chantelle@circleci.com>
2022-02-09 13:45:08 -0500

    Alter way to generate img to satisfy httpproofer rules

--- src/js/site/expandImage.js
+++ src/js/site/expandImage.js
@@ -1,31 +1,33 @@
 /*
- * All images have an expand button option
+ * Add enlarge image capability for easier readability
  */
 export function expandImageOnClick() {
   // create references to the modal...
   var modal = document.getElementById('expandModal');
+
   // get all images without class icon or logo, inside or article but also have src `.png`
   // it is most likely that a icon or logo is src `.svg`
   let images = document.querySelectorAll(
     "article img:not(a):not(.icon):not(.logo)[src*='.png']",
   );
-  // the image in the modal
-  var modalImg = document.getElementById('modalImg');
-  // and the caption in the modal
-  var captionText = document.getElementById('caption');
+
+  // create reference to add caption
+  let captionText = document.getElementById('caption');
 
   for (let i = 0; i < images.length; i++) {
     let img = images[i];
     // and attach our click listener for this image.
     img.onclick = function () {
       modal.style.display = 'block';
-      modalImg.src = this.src;
+      // add image only when source is defined because of HTTPproofer rules
+      let modalImg = document.getElementById('modalImg');
+      modalImg.innerHTML = `<img src="${this.src}" class="modal-content">`
       captionText.innerHTML = this.alt;
     };
     img.classList.add('enlarge');
   }
 
-  var span = document.getElementsByClassName('close')[0];
+  let span = document.getElementsByClassName('close')[0];
   span.onclick = function () {
     modal.style.display = 'none';
   };
